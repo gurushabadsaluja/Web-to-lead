79f8aa9515e1bd982dba77dc91077aa07ae98ace
Gurushabad Saluja <gurshabadsaluja2626@gmail.com>
2024-09-10 11:36:03 +0530

    recaptcha added

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function beforeSubmit(){
-    let inputdate = document.querySelector(".inputdate");
-    let outputdate = document.querySelector(".outputdate");
-    console.log(inputdate.value)
+let captchasubmutted = false;
 
-    let formattedDate = new Date (inputdate.value).toLocaleDateString("en-US");
-    outputdate.value = formattedDate;
+
+    function beforeSubmit(event) {
+        if(captchasubmutted){
+        let inputdate = document.querySelector(".inputdate");
+        let outputdate = document.querySelector(".outputdate");
+        console.log(inputdate.value);
+      
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+        outputdate.value = formattedDate;
+      }
+      else{
+        alert("Please check the recaptcha box");
+        event.preventdefault();
+}
+
+
+}
+
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
+
+function captchasuccess(){
+    captchasubmutted=true;
 }
